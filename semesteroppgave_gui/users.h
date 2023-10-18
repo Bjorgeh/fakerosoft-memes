fe235ef854738f0c4e743a0ab423bce5e54b15f9
bjorgeh <19abh92@gmail.com>
2023-10-18 23:06:34 +0200

    added accessability for roles
    the user now has roles. 1111 admin, 2222 teacher and 3333 student.
    They see different info on next page after login.

--- semesteroppgave_gui/users.h
+++ semesteroppgave_gui/users.h
@@ -8,14 +8,18 @@ class users : public QObject
     Q_OBJECT
 public:
     explicit users(QObject *parent = nullptr);
-    users(QString newUser,QString newPass);
+    users(QString newUser,QString newPass, int userCode);
 
     Q_INVOKABLE QString getUser();
+
     QString getPass();
+    Q_INVOKABLE int getUserCode();
+    Q_INVOKABLE int setCode(int code);
 
 private:
     QString user;
     QString password;
+    int userCode;
 
 public slots:
     void setUser(QString newUser);
